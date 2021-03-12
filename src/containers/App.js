6479804466e5b0f317ac77d74cd8912f533a927d
pascwin <winner@hm.edu>
2021-03-12 13:14:25 +0100

    change1

--- src/containers/App.js
+++ src/containers/App.js
@@ -26,25 +26,26 @@ class App extends Component {
 
     onSearchChange = (event) => {
         this.setState({ searchfield: event.target.value })
+        console.log(this.state.searchfield)
     }
+
     render() {
         const { robots, searchfield } = this.state;
         const filteredRobots = robots.filter(robot => {
             return robot.name.toLowerCase().includes(searchfield.toLowerCase())
         })
         if (!robots.length) {
-            return <h1>Loading...</h1>
+            return <h1> Loading... </h1>
         } else {
-            return (
-                <div className="tc">
-                    <h1>RoboFriends</h1>
-                    <SearchBox searchChange={this.onSearchChange} />
-                    <Scroll>
-                        <ErrorBoundry>
-                            <CardList robots={filteredRobots} />
-                        </ErrorBoundry>
-                    </Scroll>
-                </div>
+            return (<div className="tc" >
+                <h1 > RoboFriends </h1>
+                <SearchBox searchChange={this.onSearchChange} />
+                <Scroll >
+                    <ErrorBoundry >
+                        <CardList robots={filteredRobots} />
+                    </ErrorBoundry>
+                </Scroll>
+            </div>
             )
         }
     }
